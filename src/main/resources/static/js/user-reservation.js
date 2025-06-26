6aeba870beada24cd6947b22231c4559ab301963
Mint <audwl03071@gmail.com>
2025-06-26 23:25:32 +0900

    fix: alert 메세지 문제 해결

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -220,7 +220,7 @@ async function fetchReservationPayment(paymentData, reservationData) {
             });
         } else {
             response.json().then(successBody => {
-                window.alert("예약 결제 성공 : " + successBody.message)
+                window.alert("예약 결제 성공");
                 console.log("예약 결제 성공 : " + JSON.stringify(successBody));
                 window.location.reload();
             });
